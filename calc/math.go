f427af86a1d50df57da928e053c4b391c2ed1211
Valet2You <indra.jha@viralops.com>
2022-02-22 17:09:31 +0530

    v2

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,14 @@
 package calc
 
-func Add(numbers ...int) int {
+import "github.com/pkg/errors"
+
+func Add(numbers ...int) (error, int) {
 	sum := 0
+	if len(numbers) < 2 {
+		return errors.New("Please provide more then one number"), sum
+	}
 	for _, num := range numbers {
 		sum += num
 	}
-	return sum
+	return nil, sum
 }
